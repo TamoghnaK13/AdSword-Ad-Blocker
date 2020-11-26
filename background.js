6d669b15df95119eb1edd9ce507a529cf8b7095d
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2020-11-26 10:17:37 -0500

    Extension Base

---
+++ background.js
@@ -0,0 +1,11 @@
+var enabled = true;
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		if(enabled) {
+			console.log("blocking:", details.url);
+		}
+		return {cancel: enabled };
+	},
+	{urls: blocked_domains},
+	["blocking"]
+);
